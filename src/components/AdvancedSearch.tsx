bc4906075417fccb59c08e1054f989edb609565e
Jack <giacomo.deluca@hotmail.it>
2025-07-03 21:56:21 +0200

    added Buddhanexus link

--- src/components/AdvancedSearch.tsx
+++ src/components/AdvancedSearch.tsx
@@ -1,36 +1,37 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { 
-  SegmentedControl, 
-  TextInput, 
-  MultiSelect, 
-  Button, 
-  Stack, 
-  Title, 
-  Text, 
-  Paper, 
-  Loader, 
-  Accordion, 
-  Group, 
-  Badge, 
+import {
+  SegmentedControl,
+  TextInput,
+  MultiSelect,
+  Button,
+  Stack,
+  Title,
+  Text,
+  Paper,
+  Loader,
+  Accordion,
+  Group,
+  Badge,
   Divider,
   ActionIcon,
   Box,
   Collapse,
   useMantineTheme,
-  Tooltip
+  Tooltip,
+  Anchor
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { 
-  IconSearch, 
-  IconBook, 
-  IconChevronDown, 
-  IconChevronRight, 
+import {
+  IconSearch,
+  IconBook,
+  IconChevronDown,
+  IconChevronRight,
   IconExternalLink,
-  IconInfoCircle, 
+  IconInfoCircle,
   IconChevronUp,
   IconFilter,
   IconAdjustments,
-  IconX
+  IconX,
 } from '@tabler/icons-react';
 import classes from './AdvancedSearch.module.css';
 import { BookResult, SearchResult, SegmentResult } from '../types/searchTypes';
@@ -39,8 +40,6 @@ import SearchInputComponent from './AdvancedSearchInput';
 import AdvancedSearchResults from './AdvancedSearchResults';
 import { use } from 'chai';
 import filterData from '../utils/filter_data.json';
-
-
 
 interface AdvancedSearchProps {
   advancedSearchResults: SearchResult | null;
@@ -59,11 +58,22 @@ interface AdvancedSearchProps {
     toggle: () => void;
   };
   setIsNavbarVisible: (value: boolean) => void;
-
-  
 }
 
-function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSearch, onOpenText, isMobile, setTargetSegmentNumber, query, setQuery, setMatchedBookSegments, matchedBookSegments, handleAdvancedSearch, setIsNavbarVisible }: AdvancedSearchProps) {
+function AdvancedSearch({
+  advancedSearchResults,
+  setAdvancedSearchResults,
+  onSearch,
+  onOpenText,
+  isMobile,
+  setTargetSegmentNumber,
+  query,
+  setQuery,
+  setMatchedBookSegments,
+  matchedBookSegments,
+  handleAdvancedSearch,
+  setIsNavbarVisible,
+}: AdvancedSearchProps) {
   const [queryType, setQueryType] = useState('stemmed');
   const [filterMode, setFilterMode] = useState('include');
   const [searchType, setSearchType] = useState<'segments' | 'books'>('books');
@@ -71,7 +81,7 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
   const [collections, setCollections] = useState<string[]>([]);
   const [titles, setTitles] = useState<string[]>([]);
   const [limit, setLimit] = useState(500);
-  
+
   // State for handling the search process
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
@@ -79,27 +89,25 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
 
   const currentInput = useRef<string | null>(null);
 
-
   // Mock data for dropdowns - replace with actual data from API
- 
+
   // Transform titles for MultiSelect
-  const titleOptions = filterData.titles.map(item => ({
+  const titleOptions = filterData.titles.map((item) => ({
     value: item.text_id,
-    label: item.title
+    label: item.title,
   }));
 
   // Transform authors for MultiSelect
-  const authorOptions = filterData.authors.map(author => ({
+  const authorOptions = filterData.authors.map((author) => ({
     value: author,
-    label: author
+    label: author,
   }));
 
   // Transform collections for MultiSelect
-  const collectionOptions = filterData.collections.map(collection => ({
+  const collectionOptions = filterData.collections.map((collection) => ({
     value: collection,
-    label: collection
+    label: collection,
   }));
-
 
   const updateQuery = () => {
     setQuery(currentInput.current || '');
@@ -112,7 +120,6 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
     setError(null);
     setAdvancedSearchResults(null);
 
-    
     // Create the search parameters with type declaration
     const searchParams: {
       query: string;
@@ -132,7 +139,7 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
       limit: limit,
       // add references here to author, collection, and text id, change the API call as well
     };
-    
+
     try {
       // Make the API request
       const response = await fetch('https://api.yogasutratrees.com/api/search', {
@@ -142,23 +149,22 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
         },
         body: JSON.stringify(searchParams),
       });
-      
+
       if (!response.ok) {
         throw new Error(`Error ${response.status}: ${await response.text()}`);
       }
-      
+
       const data = await response.json();
-      
+
       // Set the search results based on the search type
       setAdvancedSearchResults({
         type: searchType,
-        results: data
+        results: data,
       });
-      
+
       // Also pass the results to the parent component
       onSearch(data);
       console.log('Search results:', data);
-      
     } catch (err: any) {
       setError(err.message || 'An error occurred while searching');
     } finally {
@@ -167,72 +173,99 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
   };
 
   useEffect(() => {
-    if (query.trim()) {  // Only run search if query has content
-      console.log("query launching", query)
+    if (query.trim()) {
+      // Only run search if query has content
+      console.log('query launching', query);
       handleSearch();
     }
   }, [query, searchType]);
-  
+
   // Handle clicking on a text to open it
   // here add two things: close the modal and close the navbar as well if it's on mobile
   // TODO
 
-  const handleOpenText = (textId: string, title: string, segmentNumber?: number, matchedSegments?: number[]) => {
+  const handleOpenText = (
+    textId: string,
+    title: string,
+    segmentNumber?: number,
+    matchedSegments?: number[]
+  ) => {
     if (onOpenText) {
-
       if (advancedSearchResults) {
         if (advancedSearchResults.type === 'books') {
           // Find the book in search results
           const book = advancedSearchResults.results.find((b: BookResult) => b.text_id === textId);
           if (book) {
             // Extract all segment numbers from this book
-            setMatchedBookSegments(book.segments.map(segment => segment.segment_number))
+            setMatchedBookSegments(book.segments.map((segment) => segment.segment_number));
           }
-        } 
-        else if (advancedSearchResults.type === 'segments') {
+        } else if (advancedSearchResults.type === 'segments') {
           // Find all segments that belong to this text
-          setMatchedBookSegments(advancedSearchResults.results
-            .filter((seg: SegmentResult) => seg.segment_id.split(':')[0] === textId)
-            .map((seg: SegmentResult) => seg.segment_number))
+          setMatchedBookSegments(
+            advancedSearchResults.results
+              .filter((seg: SegmentResult) => seg.segment_id.split(':')[0] === textId)
+              .map((seg: SegmentResult) => seg.segment_number)
+          );
         }
       }
-      console.log("matched_segments", matchedBookSegments);
+      console.log('matched_segments', matchedBookSegments);
       setTargetSegmentNumber(segmentNumber || 0);
       onOpenText(textId, title);
       handleAdvancedSearch.close();
       console.log(segmentNumber);
-      isMobile ? (setIsNavbarVisible(false)) : null;
+      isMobile ? setIsNavbarVisible(false) : null;
     }
   };
 
   return (
     <div className={classes.pageWrapper}>
       <Paper p="md" radius="md" className={classes.optionsContainer}>
-        <Title order={2} mb="xs" className={classes.title}>Sanskrit Corpus Search</Title>
+        <Title order={2} mb="xs" className={classes.title}>
+          Sanskrit Corpus Search
+        </Title>
         <Text size="sm" c="dimmed" mb="md">
-          Search across the Sanskrit collection with additional filters for more specific results.
+          Search across the  Sanskrit collection from{' '} 
+          
+          <Anchor
+            href="https://github.com/dharmamitra/dharmanexus"
+            target="_blank"
+            rel="noopener noreferrer"
+            c="orange"
+          >
+            Buddhanexus
+          </Anchor>{' '}
+  
+          with additional filters for more specific results.
         </Text>
-        
+
         {/* Search bar at the top */}
-        <SearchInputComponent 
+        <SearchInputComponent
           handleSearch={handleSearch}
           isLoading={isLoading}
           classes={classes}
           setQuery={setQuery}
           currentInput={currentInput}
-          
         />
 
         <Stack gap="xs">
           {/* Search type segment control */}
           <div>
-            <Group gap="lg" justify='flex-start'>
-              <Text size="sm" fw={500} mb={5} pr='7px'>Result Type  </Text>
-              <Tooltip label="Show search result grouped by books or as individual segments." position="top-end" withArrow>
-                <IconInfoCircle size={14} style={{ 
-                  color: 'var(--mantine-color-dimmed)',
-                  transform: 'translateY(-4px)'
-                }} />
+            <Group gap="lg" justify="flex-start">
+              <Text size="sm" fw={500} mb={5} pr="7px">
+                Result Type{' '}
+              </Text>
+              <Tooltip
+                label="Show search result grouped by books or as individual segments."
+                position="top-end"
+                withArrow
+              >
+                <IconInfoCircle
+                  size={14}
+                  style={{
+                    color: 'var(--mantine-color-dimmed)',
+                    transform: 'translateY(-4px)',
+                  }}
+                />
               </Tooltip>
             </Group>
             <SegmentedControl
@@ -245,20 +278,29 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
               fullWidth
               classNames={{
                 indicator: classes.indicator,
-                root: classes.root
+                root: classes.root,
               }}
             />
           </div>
 
           {/* Search mode segment control */}
           <div>
-            <Group gap="lg" justify='flex-start'>
-              <Text size="sm" fw={500} mb={5}>Search Mode</Text>
-              <Tooltip label="Select how terms are matched in the text." position="top-end" withArrow>
-                <IconInfoCircle size={14} style={{ 
-                  color: 'var(--mantine-color-dimmed)',
-                  transform: 'translateY(-4px)'
-                }} />
+            <Group gap="lg" justify="flex-start">
+              <Text size="sm" fw={500} mb={5}>
+                Search Mode
+              </Text>
+              <Tooltip
+                label="Select how terms are matched in the text."
+                position="top-end"
+                withArrow
+              >
+                <IconInfoCircle
+                  size={14}
+                  style={{
+                    color: 'var(--mantine-color-dimmed)',
+                    transform: 'translateY(-4px)',
+                  }}
+                />
               </Tooltip>
             </Group>
             <SegmentedControl
@@ -272,88 +314,83 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
               fullWidth
               classNames={{
                 indicator: classes.indicator,
-                root: classes.root
+                root: classes.root,
               }}
             />
           </div>
 
           {/* Accordion for search settings and filters */}
-          <Accordion 
-            multiple 
-            
-            value={value} 
+          <Accordion
+            multiple
+            value={value}
             onChange={setValue}
             className={classes.settingsAccordion}
             classNames={{
               root: classes.settingsAccordionRoot,
               panel: classes.settingsAccordionPanel,
               item: classes.settingsAccordionItem,
-              control: classes.settingsAccordionControl
+              control: classes.settingsAccordionControl,
             }}
           >
-            
             {/* Filters Section */}
             <Accordion.Item value="2" className={classes.accordionItem}>
               <Accordion.Control className={classes.accordionHeader}>
-              <Group justify="space-between" w="100%">
-                  <Text
-                    className= {classes.activeFilters}
-                    size="sm"
-                    >
+                <Group justify="space-between" w="100%">
+                  <Text className={classes.activeFilters} size="sm">
                     Filters
                   </Text>
-                  
+
                   {/* Show different content based on whether filters exist */}
-                  <Group 
-                  gap="xs"
-                  >
+                  <Group gap="xs">
                     {authors.length > 0 || collections.length > 0 || titles.length > 0 ? (
                       <>
-                      <Tooltip 
-                      label="Clear all filters"
-                      >
-                        <ActionIcon 
-                          size="sm" 
-                          variant="subtle" 
-                          color="gray"
-                          onClick={(e) => {
-                            e.stopPropagation(); // Prevent accordion from toggling
-                            setAuthors([]);
-                            setCollections([]);
-                            setTitles([]);
-                          }}
+                        <Tooltip label="Clear all filters">
+                          <ActionIcon
+                            size="sm"
+                            variant="subtle"
+                            color="gray"
+                            onClick={(e) => {
+                              e.stopPropagation(); // Prevent accordion from toggling
+                              setAuthors([]);
+                              setCollections([]);
+                              setTitles([]);
+                            }}
+                          >
+                            <IconX size={14} />
+                          </ActionIcon>
+                        </Tooltip>
+                        <Text
+                          className={classes.activeFilters}
+                          size="xs"
+                          c="dimmed"
+                          pr="xl"
+                          pl="md"
                         >
-                          <IconX size={14} />
-                        </ActionIcon>
-                      </Tooltip>
-                        <Text 
-                        className= {classes.activeFilters}
-                        
-                         size="xs"
-                         c="dimmed"
-                         pr="xl"
-                         pl="md">
-                          <span className={classes.filterNumber}>{authors.length + collections.length + titles.length}</span> 
+                          <span className={classes.filterNumber}>
+                            {authors.length + collections.length + titles.length}
+                          </span>
                           active filters
                         </Text>
-                        
                       </>
                     ) : (
-                      <Text 
-                      className= {classes.activeFilters}
-                      size="xs" 
-                      c="dimmed" 
-                      pr= "lg"
-                      style={{
-                        fontStyle: "italic"
-                      }}>No active filters</Text>
+                      <Text
+                        className={classes.activeFilters}
+                        size="xs"
+                        c="dimmed"
+                        pr="lg"
+                        style={{
+                          fontStyle: 'italic',
+                        }}
+                      >
+                        No active filters
+                      </Text>
                     )}
                   </Group>
                 </Group>
               </Accordion.Control>
-              
+
               <Accordion.Panel className={classes.accordionPanel}>
-                 {/* <SegmentedControl
+                {/* <SegmentedControl
                   value={filterMode}
                   onChange={setFilterMode}
                   data={[
@@ -368,7 +405,7 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
                   mb={16}
                   mt={16}
                 /> */}
-                
+
                 <div className={classes.inputContainer}>
                   {!isMobile && (
                     <Text size="sm" fw={500} className={classes.fieldLabel}>
@@ -383,7 +420,7 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
                     searchable
                     clearable
                     className={classes.fieldInput}
-                    label={isMobile ? "Titles" : undefined}
+                    label={isMobile ? 'Titles' : undefined}
                     size="xs"
                   />
                 </div>
@@ -402,7 +439,7 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
                     searchable
                     clearable
                     className={classes.fieldInput}
-                    label={isMobile ? "Authors" : undefined}
+                    label={isMobile ? 'Authors' : undefined}
                     size="xs"
                   />
                 </div>
@@ -421,7 +458,7 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
                     searchable
                     clearable
                     className={classes.fieldInput}
-                    label={isMobile ? "Collections" : undefined}
+                    label={isMobile ? 'Collections' : undefined}
                     size="xs"
                   />
                 </div>
@@ -429,10 +466,10 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
             </Accordion.Item>
           </Accordion>
 
-          <Button 
+          <Button
             className={classes.searchButton}
-            onClick={() => updateQuery()} 
-            size="md" 
+            onClick={() => updateQuery()}
+            size="md"
             mt="sm"
             loaderProps={{ type: 'dots' }}
             leftSection={<IconSearch size={14} />}
@@ -454,6 +491,5 @@ function AdvancedSearch({ advancedSearchResults, setAdvancedSearchResults, onSea
     </div>
   );
 }
-
 
 export default AdvancedSearch;
